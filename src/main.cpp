c884ffc78e242be429318226e196e470adbf7254
Brian White <brian@thirdember.com>
2019-08-27 20:38:28 -0400

    initial commit with working code

---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+#define BLYNK_PRINT Serial
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <Adafruit_MAX31856.h>
+
+#ifdef KILN_BLYNK_AUTH
+  char auth[] = KILN_BLYNK_AUTH;
+#endif
+
+#ifdef KILN_WIFI_SSID
+  char ssid[] = KILN_WIFI_SSID;
+#endif
+
+#ifdef KILN_WIFI_PWD
+  char pass[] = KILN_WIFI_PWD;
+#endif
+
+BlynkTimer timer;
+Adafruit_MAX31856 maxthermo = Adafruit_MAX31856(12, 13, 14, 15);
+
+bool notifiedMaxTemp = false;
+float maxTempForNotify = 80.0;
+
+float ConvertCelsiusToFahrenheit(float temperatureInCelsius) {
+  return (temperatureInCelsius * 9.0 / 5.0) + 32.0;
+}
+
+bool HasFault(uint8_t fault)
+{
+    if (fault) {
+      if (fault & MAX31856_FAULT_CJRANGE) Serial.println("Cold Junction Range Fault");
+      if (fault & MAX31856_FAULT_TCRANGE) Serial.println("Thermocouple Range Fault");
+      if (fault & MAX31856_FAULT_CJHIGH)  Serial.println("Cold Junction High Fault");
+      if (fault & MAX31856_FAULT_CJLOW)   Serial.println("Cold Junction Low Fault");
+      if (fault & MAX31856_FAULT_TCHIGH)  Serial.println("Thermocouple High Fault");
+      if (fault & MAX31856_FAULT_TCLOW)   Serial.println("Thermocouple Low Fault");
+      if (fault & MAX31856_FAULT_OVUV)    Serial.println("Over/Under Voltage Fault");
+      if (fault & MAX31856_FAULT_OPEN)    Serial.println("Thermocouple Open Fault");
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+    
+}
+
+void TemperatureTimeProcess() 
+{ 
+  if (!HasFault(maxthermo.readFault())) {
+    float kilnTempInCelsius = maxthermo.readThermocoupleTemperature();
+    float kilnTempInFahrenheit = ConvertCelsiusToFahrenheit(kilnTempInCelsius);
+
+    float boardTempInCelsius = maxthermo.readCJTemperature();
+    float boardTempInFahrenheit = ConvertCelsiusToFahrenheit(boardTempInCelsius);
+
+    Serial.println(kilnTempInFahrenheit);
+    Blynk.virtualWrite(V5, kilnTempInFahrenheit);
+    Blynk.virtualWrite(V6, boardTempInFahrenheit);
+
+    if (kilnTempInFahrenheit >= maxTempForNotify && !notifiedMaxTemp)
+    {
+      Blynk.notify("Max Temp Target Reached");
+      notifiedMaxTemp = true;
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+
+  timer.setInterval(1000L, TemperatureTimeProcess);
+  
+  maxthermo.begin();
+  maxthermo.setThermocoupleType(MAX31856_TCTYPE_K);
+}
+
+void loop() {
+  Blynk.run();
+  timer.run(); 
+}
